db7beb16d6fc389576185f27acc5bc087c12bcef
Barış Baba <barismertciloglu2@gmail.com>
2026-07-22 16:03:33 +0300

    Add basic auth for admin panel

--- server.js
+++ server.js
@@ -51,6 +51,19 @@ const uploadImages = multer({
   }
 });
 
+// Yönetim Paneli Şifre Koruması
+app.use('/admin.html', (req, res, next) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  if (login === 'soner' && password === 'soner7610') {
+    return next();
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="401"');
+  res.status(401).send('Yetkisiz erişim.');
+});
+
 // Statik Dosyalar (Frontend ve Admin)
 app.use(express.static(path.join(__dirname, 'public')));
 
